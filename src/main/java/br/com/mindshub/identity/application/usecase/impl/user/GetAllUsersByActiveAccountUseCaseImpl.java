bdaee4f94db0d1b4129f730fd677c0754fd65d92
pdrotmz <pedrotomaz0210@gmail.com>
2026-09-16 11:35:31 -0300

    refactor(identity): standardize user use case implementation names

--- src/main/java/br/com/mindshub/identity/application/usecase/impl/user/GetAllUsersByActiveAccountUseCaseImpl.java
+++ src/main/java/br/com/mindshub/identity/application/usecase/impl/user/GetAllUsersByActiveAccountUseCaseImpl.java
@@ -1,7 +1,6 @@
 package br.com.mindshub.identity.application.usecase.impl.user;
 
-import br.com.mindshub.identity.application.usecase.user.GetUsersByRoleUseCase;
-import br.com.mindshub.identity.domain.enums.Role;
+import br.com.mindshub.identity.application.usecase.user.GetAllUsersByActiveAccount;
 import br.com.mindshub.identity.domain.model.User;
 import br.com.mindshub.identity.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +10,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class GetUsersByRoleImpl implements GetUsersByRoleUseCase {
+public class GetAllUsersByActiveAccountUseCaseImpl implements GetAllUsersByActiveAccount {
 
     private final UserRepository userRepository;
 
+
     @Override
-    public List<User> execute(Role role) {
-        return userRepository.findByRole(role);
+    public List<User> execute(boolean isActive) {
+        return userRepository.findByActive(isActive);
     }
 }
--- src/main/java/br/com/mindshub/identity/application/usecase/impl/user/GetUsersByRoleUseCaseImpl.java
+++ src/main/java/br/com/mindshub/identity/application/usecase/impl/user/GetUsersByRoleUseCaseImpl.java
@@ -1,9 +1,9 @@
 package br.com.mindshub.identity.application.usecase.impl.user;
 
-import br.com.mindshub.identity.application.usecase.user.GetAllUsersByActiveAccount;
+import br.com.mindshub.identity.application.usecase.user.GetUsersByRoleUseCase;
+import br.com.mindshub.identity.domain.enums.Role;
 import br.com.mindshub.identity.domain.model.User;
 import br.com.mindshub.identity.domain.repository.UserRepository;
-import br.com.mindshub.identity.presentation.dto.response.user.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +11,12 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class GetAllUsersByActiveAccountImpl implements GetAllUsersByActiveAccount {
+public class GetUsersByRoleUseCaseImpl implements GetUsersByRoleUseCase {
 
     private final UserRepository userRepository;
 
-
     @Override
-    public List<User> execute(boolean isActive) {
-        return userRepository.findByActive(isActive);
+    public List<User> execute(Role role) {
+        return userRepository.findByRole(role);
     }
 }
